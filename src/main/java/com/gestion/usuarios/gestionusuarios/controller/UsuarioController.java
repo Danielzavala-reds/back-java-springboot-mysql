84c579759349e7ccfffa3e3a94b6ea1b3e0f14a0
Your Name <you@exle.com>
2023-04-03 16:04:39 -0600

    correcciones en controlador

--- src/main/java/com/gestion/usuarios/gestionusuarios/controller/UsuarioController.java
+++ src/main/java/com/gestion/usuarios/gestionusuarios/controller/UsuarioController.java
@@ -1,58 +1,67 @@
-package com.gestion.usuarios.gestionusuarios.controller;
+    package com.gestion.usuarios.gestionusuarios.controller;
 
 
 import java.util.List;
 import java.util.Optional;
 
+import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.gestion.usuarios.gestionusuarios.excepcions.NotFound;
 import com.gestion.usuarios.gestionusuarios.model.Usuario;
 import com.gestion.usuarios.gestionusuarios.repository.UsuarioRepository;
 
 @RestController
+@RequestMapping("/api/") /* Notacion para dar un prefijo a la URL */
 public class UsuarioController {
     
     @Autowired UsuarioRepository uRepository;
 
-    @GetMapping(value = "/api/usuarios")
+    @GetMapping(value = "/usuarios")
     public List<Usuario> getUsuarios(){
         return uRepository.findAll();
     }
 
-    @GetMapping(value = "/api/usuarios/{id}")
-    public Optional<Usuario> getUsuario(@PathVariable Long id){
-        return uRepository.findById(id);
+    @GetMapping(value = "/usuarios/{id}")
+    public ResponseEntity<Usuario> getUsuario(@PathVariable Long id){
+       Usuario usuario = uRepository.findById(id)
+                         .orElseThrow( () -> new NotFound("No existe el usuario con el id: " + id));
+        
+        return ResponseEntity.ok( usuario);
     } 
 
-    @PostMapping(value = "/api/nuevo-usuario")
-    public String nuevoUsuario(@RequestBody Usuario usuario){
-        uRepository.save(usuario);
+    @PostMapping(value = "/nuevo-usuario")
+    public Usuario nuevoUsuario(@RequestBody Usuario usuario){
         
-        return "";
+        return uRepository.save(usuario);
     }
 
-    @PutMapping(value = "/api/editar-usuario/{id}")
-    public String editarUsuario(@PathVariable Long id ,@RequestBody Usuario usuario){
-        Usuario actualizar = uRepository.findById(id).get();
-        actualizar.setNombre(usuario.getNombre());
-        actualizar.setApellido(usuario.getApellido());
-        actualizar.setEmail(usuario.getEmail());
-        actualizar.setTelefono(usuario.getTelefono());
-        actualizar.setPassword(usuario.getPassword());
+    @PutMapping(value = "/editar-usuario/{id}")
+    public ResponseEntity<Usuario> editarUsuario(@PathVariable Long id ,@RequestBody Usuario usuarioActualizar){
 
-        uRepository.save(actualizar);
+        Usuario usuario = uRepository.findById(id)
+        .orElseThrow( () -> new NotFound("No existe el usuario con el id: " + id));
 
-        return "";
+        usuario.setNombre(usuarioActualizar.getNombre());
+        usuario.setApellido(usuarioActualizar.getApellido());
+        usuario.setEmail(usuarioActualizar.getEmail());
+        usuario.setTelefono(usuarioActualizar.getTelefono());
+        usuario.setPassword(usuarioActualizar.getPassword());
+        
+
+        return ResponseEntity.ok(usuario);
     }
 
-    @DeleteMapping(value = "/api/usuarios/{id}")
+    @DeleteMapping(value = "/usuarios/{id}")
     public void deleteUsuario(@PathVariable Long id){
         uRepository.deleteById(id);
     }
